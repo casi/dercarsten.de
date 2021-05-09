3ab681500fd513ba748c3606794eb51c983b1961
Carsten Behnert <c.behnert@gmx.de>
2021-05-09 02:01:02 +0200

    Add some new fancy shit
    * Bump ruby from 3.0.0 to 3.0.1
    * Update gems and dependencies
    * Improve layout and style and add normalize.css

--- lib/random_content.rb
+++ lib/random_content.rb
@@ -7,8 +7,7 @@ From "As You Like It"
 "All the world\'s a stage,
 and all the men and women merely players;
 They have their exits and their entrances;
-And one man in his time plays many parts,
-His acts being seven ages. [..]"
+And one man in his time plays many parts [..]"
 
 - Jaques
 ',
